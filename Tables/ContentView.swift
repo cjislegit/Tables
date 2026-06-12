140866f0e4281302e20fffeb4cde6342b81e05ac
cjislegit <carlosjramirez11@gmail.com>
2026-06-11 20:31:58 -0700

    Initial Commit

---
+++ Tables/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tables
+//
+//  Created by Carlos Ramirez on 6/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tables/TablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TablesApp.swift
+//  Tables
+//
+//  Created by Carlos Ramirez on 6/11/26.
+//
+
+import SwiftUI
+
+@main
+struct TablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
