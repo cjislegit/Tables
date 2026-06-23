1feac1f6006b9f71481fc2a560fe928bd9bff8f4
cjislegit <carlosjramirez11@gmail.com>
2026-06-22 19:49:31 -0700

    Added logic for picking questions

--- Tables/ContentView.swift
+++ Tables/ContentView.swift
@@ -8,6 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var maxTable = 12
+    
+    struct Question {
+        let table: Int
+        let mutilpier: Int
+        
+        var text: String {
+            "\(table) * \(mutilpier)"
+        }
+        
+        var answer: Int {
+            table * mutilpier
+        }
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -15,7 +30,33 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
+        .onAppear(perform: StartGame)
         .padding()
+    }
+    
+    func StartGame() {
+        if let questionsURL = Bundle.main.url(forResource: "questions", withExtension: "txt") {
+            if let questions = try? String(contentsOf: questionsURL, encoding: .utf8) {
+                let allQuestions = questions.split(separator: "\n")
+                let questionObject = allQuestions.compactMap { question -> Question? in
+                    let parts = question.components(separatedBy: " ")
+                    
+                    guard parts.count == 3,
+                        let table = Int(parts[0]),
+                        let multiplier = Int(parts[2])
+                    else { return nil }
+                    
+                    return Question(table: table, mutilpier: multiplier)
+                }
+                
+                let allowedQuestions = questionObject.filter {
+                    $0.table <= maxTable
+                }
+                
+                let randomQuestion = allowedQuestions.randomElement()!
+                print(randomQuestion.text)
+            }
+        }
     }
 }
 
